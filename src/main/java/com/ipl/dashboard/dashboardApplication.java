5eb037940e363e06b362acd8398e3495f77f16f1
unknown <nsweta14cs06@gmailc.com>
2022-05-12 18:48:01 +0530

    Ipl dashboard skelton

---
+++ src/main/java/com/ipl/dashboard/dashboardApplication.java
@@ -0,0 +1,13 @@
+package com.ipl.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class dashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(dashboardApplication.class, args);
+	}
+
+}
